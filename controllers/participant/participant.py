234b3257a1b6a9abf9a16575e48633e57284fae0
Katsuki Oto <katsuki.oto@dena.com>
2023-05-22 22:46:55 +0900

    experiment: forward -> rotate

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -35,7 +35,7 @@ class Wrestler (Robot):
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
         time_step = int(self.getBasicTimeStep())
         while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            action = 'Forwards' #'TurnLeft180' if self.count % 3 != 2 else 'Forwards'
+            action = 'Forwards' if self.count == 0 else 'TurnLeft180'
             self.count += 1
             motion_library.play(action)
 
