75c74d408625e9af839dc4f597e80e9aec82e767
Katsuki Oto <katsuki.oto@dena.com>
2023-05-23 04:49:49 +0900

    remove fsm

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -21,49 +21,17 @@ import sys
 # We provide a set of utilities to help you with the development of your controller. You can find them in the utils folder.
 # If you want to see a list of examples that use them, you can go to https://github.com/cyberbotics/wrestling#demo-robot-controllers
 sys.path.append('..')
-from utils.accelerometer import Accelerometer
-from utils.finite_state_machine import FiniteStateMachine
-from utils.motion_library import MotionLibrary
-from utils.current_motion_manager import CurrentMotionManager
+from ..utils.accelerometer import Accelerometer
+from ..utils.motion_library import MotionLibrary
+from ..utils.current_motion_manager import CurrentMotionManager
 
 
-class Wrestler (Robot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.count = 0
-
-    def run(self):
-        # to load all the motions from the motions folder, we use the MotionLibrary class:
-        motion_library = MotionLibrary()
-        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
-        time_step = int(self.getBasicTimeStep())
-        fall_detection = FallDetection(time_step, self)
-
-        while self.step(time_step) != -1:  # mandatory function to make the simulation run
-            if fall_detection.check()
-
-            action = 'SideStepRight' if self.count < 5 else 'Forwards'
-            self.count += 1
-            motion_library.play(action)
-
-
-class David (Robot):
+class Wrestler(Robot):
     def __init__(self):
         Robot.__init__(self)
 
         # retrieves the WorldInfo.basicTimeTime (ms) from the world file
         self.time_step = int(self.getBasicTimeStep())
-        # the Finite State Machine (FSM) is a way of representing a robot's behavior as a sequence of states
-        self.fsm = FiniteStateMachine(
-            states=['DEFAULT', 'BLOCKING_MOTION', 'FRONT_FALL', 'BACK_FALL'],
-            initial_state='DEFAULT',
-            actions={
-                'BLOCKING_MOTION': self.pending,
-                'DEFAULT': self.walk,
-                'FRONT_FALL': self.front_fall,
-                'BACK_FALL': self.back_fall
-            }
-        )
         self.accelerometer = Accelerometer(self, self.time_step)
         self.leds = {
             'right': self.getDevice('Face/Led/Right'),
@@ -73,6 +41,8 @@ class David (Robot):
         # Shoulder roll motors for getting up from a side fall
         self.RShoulderRoll = self.getDevice('RShoulderRoll')
         self.LShoulderRoll = self.getDevice('LShoulderRoll')
+
+        self.status = 'DEFAULT'
 
         # load motion files
         self.current_motion = CurrentMotionManager()
@@ -86,17 +56,28 @@ class David (Robot):
         self.fsm.transition_to('BLOCKING_MOTION')
 
         while self.step(self.time_step) != -1:
-
             self.detect_fall()
-            self.fsm.execute_action()
+            if self.status == 'FRONT_FALL':
+                self.current_motion.set(self.library.get('GetUpFront'))
+                self.status = 'BLOCKING'
+            elif self.status == 'BACK_FALL':
+                self.current_motion.set(self.library.get('GetUpBack'))
+                self.status = 'BLOCKING'
+            elif self.status == 'BLOCKING':
+                if self.current_motion.is_over():
+                    self.status = 'DEFAULT'
+
+            if self.status == 'DEFAULT':
+                if self.current_motion.get() != self.library.get('ForwardLoop'):
+                    self.current_motion.set(self.library.get('ForwardLoop'))
 
     def detect_fall(self):
         '''Detect a fall and update the FSM state.'''
         [acc_x, acc_y, _] = self.accelerometer.get_new_average()
         if acc_x < -7:
-            self.fsm.transition_to('FRONT_FALL')
+            self.status = 'FRONT_FALL'
         elif acc_x > 7:
-            self.fsm.transition_to('BACK_FALL')
+            self.status = 'BACK_FALL'
         if acc_y < -7:
             # Fell to its right, pushing itself on its back
             self.RShoulderRoll.setPosition(-1.2)
@@ -104,24 +85,6 @@ class David (Robot):
             # Fell to its left, pushing itself on its back
             self.LShoulderRoll.setPosition(1.2)
 
-    def pending(self):
-        # waits for the current motion to finish before doing anything else
-        if self.current_motion.is_over():
-            self.fsm.transition_to('DEFAULT')
-
-    def walk(self):
-        if self.current_motion.get() != self.library.get('ForwardLoop'):
-            self.current_motion.set(self.library.get('ForwardLoop'))
-
-    def front_fall(self):
-        self.current_motion.set(self.library.get('GetUpFront'))
-        self.fsm.transition_to('BLOCKING_MOTION')
-
-    def back_fall(self):
-        self.current_motion.set(self.library.get('GetUpBack'))
-        self.fsm.transition_to('BLOCKING_MOTION')
-
 # create the Robot instance and run main loop
-wrestler = David()
-
+wrestler = Wrestler()
 wrestler.run()
